c990620f31c569d43475b189ca6249043fae61d6
Fabio Fognani <fabio.fognani@gmail.com>
2024-10-28 15:03:10 +0100

    feat: query params

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import { Textarea } from "./components/ui/textarea";
 import { clsx } from "clsx";
 
@@ -9,10 +9,29 @@ function splitChars(text: string) {
 function normalize(text:string) {
   return text.toUpperCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
 }
+const textQueryParamKey = "t";
+const enegremQueryParamKey = "e";
 
 function App() {
-  const [text, setText] = useState("");
-  const [enegrem, setenegrem] = useState("");
+  const href = window.location.href;
+  const url = useMemo(() => new URL(href), [href]);
+
+  const [text, setText] = useState(url.searchParams.get(textQueryParamKey) ?? "");
+  const [enegrem, setEnegrem] = useState(url.searchParams.get(enegremQueryParamKey) ?? "");
+
+  useEffect(() => {
+    if (text) {
+      url.searchParams.set(textQueryParamKey, text);
+    } else {
+      url.searchParams.delete(textQueryParamKey)
+    }
+    if (enegrem) {
+      url.searchParams.set(enegremQueryParamKey, enegrem);
+    } else {
+      url.searchParams.delete(enegremQueryParamKey)
+    }
+    window.history.pushState(null, '', url.toString());
+  }, [enegrem, text, url]);
 
   const { diff, isComplete, textRemainingLetters } = useMemo(() => {
     const normalizedenegrem = normalize(enegrem);
@@ -60,28 +79,30 @@ function App() {
         ENEGREM
       </h1>
       <Textarea placeholder="Insert text (i.e. Codroipo)" className="text-lg min-h-24" value={text} onChange={(e) => setText(e.target.value)} />
-      {isComplete ? <h2 className="text-xl font-bold text-green-500 py-[3px]">ENEGREM COMPLETED!</h2> : <>
-        <ul className="flex items-center gap-2 flex-wrap">
-          {Object.entries(diff).map(([letter, value]) => (
-            <li
-              className={clsx(
-                "border pl-3 pr-2 py-1 gap-1 rounded-full whitespace-nowrap flex items-center",
-                value === 0 && "bg-green-200 opacity-75",
-                value < 0 && "bg-red-200"
-              )}
-              key={letter}
-            >
-              <strong>{letter}</strong> : <div className="min-w-[2ch] text-center">{value > 100 ? "99+" : value}</div>
-            </li>
-          ))}
+     
+      <ul className="flex items-center gap-2 flex-wrap">
+        {Object.entries(diff).map(([letter, value]) => (
+          <li
+            className={clsx(
+              "border pl-3 pr-2 py-1 gap-1 rounded-full whitespace-nowrap flex items-center",
+              value === 0 && "bg-green-200 opacity-75",
+              value < 0 && "bg-red-200"
+            )}
+            key={letter}
+          >
+            <strong>{letter}</strong> : <div className="min-w-[2ch] text-center">{value > 100 ? "99+" : value}</div>
+          </li>
+        ))}
         </ul>
+        {isComplete ? (
+          <h2 className="text-xl font-bold text-green-500">ENEGREM COMPLETED!</h2> 
+        ) : (
+          <ul className="flex flex-wrap items-center gap-1 py-[2px]">
+            {textRemainingLetters.map((l, i) => <li key={i}>{l === null ? "_" : l}</li>)}
+          </ul>
+        )}
 
-        <ul className="flex flex-wrap items-center gap-1">
-          {textRemainingLetters.map((l, i) => <li key={i}>{l === null ? "_" : l}</li>)}
-        </ul>
-      </>}
-
-      <Textarea disabled={!text} className="text-lg min-h-24" value={enegrem} onChange={(e) => setenegrem(e.target.value)} />
+      <Textarea disabled={!text} className="text-lg min-h-24" value={enegrem} onChange={(e) => setEnegrem(e.target.value)} />
     </div>
   )
 }
